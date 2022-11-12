d756dba8056f74859fd995e875e863d101f96800
Aktindo <akshit.singla.dps@gmail.com>
2022-11-12 10:19:47 +0530

    chore: add meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html data-theme="dracula">
-      <Head />
+      <Head>
+        <meta property="og:title" content="Quizlet" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://quizlet.aktindo.vercel.app" />
+        <meta
+          property="og:image"
+          content="https://quizlet.aktindo.vercel.app/favicon.ico"
+        />
+        <meta
+          property="og:description"
+          content="A simple API-based quiz app."
+        />
+        <meta name="theme-color" content="	#bf95f9" />
+      </Head>
       <body>
         <Main />
         <NextScript />
